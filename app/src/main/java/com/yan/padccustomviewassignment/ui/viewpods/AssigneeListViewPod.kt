a4df6128059210d2ce1a8422083142c0dbd61528
yan-naing-phyo <yannaingphyo.dev@gmail.com>
2023-04-19 13:30:11 +0630

    Add AssigneeListViewPod

--- app/src/main/java/com/yan/padccustomviewassignment/ui/viewpods/AssigneeListViewPod.kt
+++ app/src/main/java/com/yan/padccustomviewassignment/ui/viewpods/AssigneeListViewPod.kt
@@ -5,20 +5,40 @@ import android.util.AttributeSet
 import android.widget.RelativeLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.yan.padccustomviewassignment.data.vos.AssigneeVO
-import com.yan.padccustomviewassignment.databinding.ViewPodAssigneeBinding
+import com.yan.padccustomviewassignment.databinding.ViewPodAssigneeListBinding
 import com.yan.padccustomviewassignment.ui.adapters.AssigneeAdapter
+import com.yan.padccustomviewassignment.ui.delegates.ProfileDelegate
 import com.yan.padccustomviewassignment.ui.utils.OverlapItemDecoration
+import com.yan.padccustomviewassignment.utils.dpToPx
 
-class AssigneeViewPod @JvmOverloads constructor(
+class AssigneeListViewPod @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : RelativeLayout(context, attrs) {
-    private lateinit var mBinding: ViewPodAssigneeBinding
-    private val mAssigneeAdapter: AssigneeAdapter by lazy { AssigneeAdapter() }
+    companion object {
+        private const val DEFAULT_AVATAR_SIZE_IN_DP = 60f
+    }
+
+    private lateinit var mBinding: ViewPodAssigneeListBinding
+    private lateinit var mProfileDelegate: ProfileDelegate
+    private var mAvatarSize = 0f
+    private val mAssigneeAdapter: AssigneeAdapter by lazy {
+        AssigneeAdapter(
+            mAvatarSize.toInt(),
+            mProfileDelegate
+        )
+    }
 
     override fun onFinishInflate() {
         super.onFinishInflate()
-        mBinding = ViewPodAssigneeBinding.bind(this)
+        mBinding = ViewPodAssigneeListBinding.bind(this)
+    }
 
+    fun setUp(
+        avatarSize: Float = context.dpToPx(DEFAULT_AVATAR_SIZE_IN_DP),
+        profileDelegate: ProfileDelegate
+    ) {
+        mAvatarSize = avatarSize
+        mProfileDelegate = profileDelegate
         setUpAssigneeRecyclerView()
     }
 
