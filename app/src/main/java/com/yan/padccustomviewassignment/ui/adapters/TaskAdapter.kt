af0e24eef02c7ab201071200a0d38570e13883eb
yan-naing-phyo <yannaingphyo.dev@gmail.com>
2023-04-19 13:18:36 +0630

    Add TaskAdapter

--- app/src/main/java/com/yan/padccustomviewassignment/ui/adapters/TaskAdapter.kt
+++ app/src/main/java/com/yan/padccustomviewassignment/ui/adapters/TaskAdapter.kt
@@ -1,6 +1,31 @@
 package com.yan.padccustomviewassignment.ui.adapters
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import com.yan.padccustomviewassignment.data.vos.TaskVO
+import com.yan.padccustomviewassignment.databinding.ViewHolderTaskBinding
+import com.yan.padccustomviewassignment.ui.viewholders.TaskViewHolder
 
-class TaskAdapter: ListAdapter<> {
+class TaskAdapter : ListAdapter<TaskVO, TaskViewHolder>(TaskDiffUtil()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val binding = ViewHolderTaskBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return TaskViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val task = getItem(position)
+        holder.bind(task)
+    }
+}
+
+class TaskDiffUtil : DiffUtil.ItemCallback<TaskVO>() {
+    override fun areItemsTheSame(oldTask: TaskVO, newTask: TaskVO): Boolean =
+        oldTask.id == newTask.id
+
+    override fun areContentsTheSame(oldTask: TaskVO, newTask: TaskVO): Boolean =
+        oldTask == newTask
 }
