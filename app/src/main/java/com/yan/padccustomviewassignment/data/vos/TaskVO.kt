a6b578b3ff266e690581da2b13aaeb22a86e5784
yan-naing-phyo <yannaingphyo.dev@gmail.com>
2023-04-19 13:16:57 +0630

    Add TaskVO

--- app/src/main/java/com/yan/padccustomviewassignment/data/vos/TaskVO.kt
+++ app/src/main/java/com/yan/padccustomviewassignment/data/vos/TaskVO.kt
@@ -1,13 +1,26 @@
 package com.yan.padccustomviewassignment.data.vos
 
-enum class TaskStatus {
-    IN_PROGRESS, DONE, REVIEW
+import android.graphics.Color
+
+private val COLOR_IN_PROGRESS = Color.argb(255, 231, 111, 81)
+private val COLOR_DONE = Color.argb(255, 2, 62, 138)
+private val COLOR_REVIEW = Color.argb(255, 109, 104, 117)
+
+enum class TaskStatus(val color: Int, val status: String) {
+    IN_PROGRESS(
+        color = COLOR_IN_PROGRESS,
+        status = "In progress"
+    ),
+    DONE(color = COLOR_DONE, status = "Done"),
+    REVIEW(color = COLOR_REVIEW, status = "Review")
 }
 
 data class TaskVO(
+    val assigneeList: List<AssigneeVO>,
     val attachedFiles: List<AttachedFileVO> = emptyList(),
     val comments: List<CommentVO> = emptyList(),
     val id: Int,
+    val tag: String,
     val taskName: String,
     val taskStatus: TaskStatus = TaskStatus.IN_PROGRESS
 )
