c578ebfa1c6616840e8b78e30a8d797c6402ee62
yan-naing-phyo <yannaingphyo.dev@gmail.com>
2023-04-19 13:18:09 +0630

    Implement TaskModel

--- app/src/main/java/com/yan/padccustomviewassignment/data/models/TaskModelImpl.kt
+++ app/src/main/java/com/yan/padccustomviewassignment/data/models/TaskModelImpl.kt
@@ -1,13 +1,31 @@
 package com.yan.padccustomviewassignment.data.models
 
+import com.yan.padccustomviewassignment.data.vos.AssigneeVO
 import com.yan.padccustomviewassignment.data.vos.TaskStatus
 import com.yan.padccustomviewassignment.data.vos.TaskVO
 import io.reactivex.rxjava3.core.Observable
 
 private val tasks = listOf(
-    TaskVO(id = 1, taskName = "Contact page"),
-    TaskVO(id = 2, taskName = "Calculator page", taskStatus = TaskStatus.DONE),
-    TaskVO(id = 3, taskName = "Technical task", taskStatus = TaskStatus.REVIEW)
+    TaskVO(
+        id = 1,
+        taskName = "Contact page",
+        tag = "#Design",
+        assigneeList = listOf(AssigneeVO.MOCK, AssigneeVO.MOCK)
+    ),
+    TaskVO(
+        id = 2,
+        taskName = "Calculator page",
+        taskStatus = TaskStatus.DONE,
+        tag = "#Design",
+        assigneeList = listOf(AssigneeVO.MOCK)
+    ),
+    TaskVO(
+        id = 3,
+        taskName = "Technical task",
+        taskStatus = TaskStatus.REVIEW,
+        tag = "#Frontend",
+        assigneeList = listOf(AssigneeVO.MOCK, AssigneeVO.MOCK)
+    )
 )
 
 object TaskModelImpl : TaskModel {
