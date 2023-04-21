6bf5abe0bfa207d6ae00a429c344c0ddfbfb0f85
yan-naing-phyo <yannaingphyo.dev@gmail.com>
2023-04-21 07:28:52 +0630

    Add xml for contact fragment

---
+++ app/src/main/java/com/yan/padccustomviewassignment/ui/fragments/ContactFragment.kt
@@ -0,0 +1,7 @@
+package com.yan.padccustomviewassignment.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.yan.padccustomviewassignment.R
+
+class ContactFragment : Fragment(R.layout.fragment_contact) {
+}
