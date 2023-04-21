aeee1a878e4937179be301459f6bcc2204033a75
yan-naing-phyo <yannaingphyo.dev@gmail.com>
2023-04-21 15:28:12 +0630

    Update MainPresenter.kt

--- app/src/main/java/com/yan/padccustomviewassignment/ui/mvp/presenters/MainPresenter.kt
+++ app/src/main/java/com/yan/padccustomviewassignment/ui/mvp/presenters/MainPresenter.kt
@@ -1,5 +1,9 @@
 package com.yan.padccustomviewassignment.ui.mvp.presenters
 
-interface MainPresenter {
+import com.yan.padccustomviewassignment.ui.delegates.ProfileDelegate
+import com.yan.padccustomviewassignment.ui.mvp.views.MainView
+
+interface MainPresenter : ProfileDelegate {
     fun onUiReady()
+    fun initView(view: MainView)
 }
