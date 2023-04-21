1e9330cef6d1b2277f98c53348d8c65ac5a3598d
yan-naing-phyo <yannaingphyo.dev@gmail.com>
2023-04-21 07:29:46 +0630

    Add kotlin file for about me fragment

---
+++ app/src/main/java/com/yan/padccustomviewassignment/ui/fragments/AboutMeFragment.kt
@@ -0,0 +1,7 @@
+package com.yan.padccustomviewassignment.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.yan.padccustomviewassignment.R
+
+class AboutMeFragment : Fragment(R.layout.fragment_about_me) {
+}
