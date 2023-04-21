8c8a705a15f3ca93fd69701293c8fb660485525e
yan-naing-phyo <yannaingphyo.dev@gmail.com>
2023-04-21 15:30:36 +0630

    Update TaskViewHolder.kt

--- app/src/main/java/com/yan/padccustomviewassignment/ui/viewholders/TaskViewHolder.kt
+++ app/src/main/java/com/yan/padccustomviewassignment/ui/viewholders/TaskViewHolder.kt
@@ -4,13 +4,20 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.yan.padccustomviewassignment.R
 import com.yan.padccustomviewassignment.data.vos.TaskVO
 import com.yan.padccustomviewassignment.databinding.ViewHolderTaskBinding
+import com.yan.padccustomviewassignment.ui.delegates.ProfileDelegate
 
-class TaskViewHolder(private val binding: ViewHolderTaskBinding) : ViewHolder(binding.root) {
+class TaskViewHolder(
+    private val binding: ViewHolderTaskBinding,
+    private val mProfileDelegate: ProfileDelegate
+) : ViewHolder(binding.root) {
     private val mAssigneeListViewPod = binding.viewPodAssigneeList.root
 
     init {
         val context = binding.root.context
-        mAssigneeListViewPod.setUp(context.resources.getDimension(R.dimen.margin_xxlarge))
+        mAssigneeListViewPod.setUp(
+            context.resources.getDimension(R.dimen.margin_xxlarge),
+            profileDelegate = mProfileDelegate
+        )
     }
 
     fun bind(taskVO: TaskVO) {
