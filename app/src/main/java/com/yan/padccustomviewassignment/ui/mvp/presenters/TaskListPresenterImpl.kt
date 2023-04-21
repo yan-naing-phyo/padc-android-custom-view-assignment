0cbe92b88afd70691493155a8063bbfd9b7ced98
yan-naing-phyo <yannaingphyo.dev@gmail.com>
2023-04-21 07:31:06 +0630

    Implement task list presenter

---
+++ app/src/main/java/com/yan/padccustomviewassignment/ui/mvp/presenters/TaskListPresenterImpl.kt
@@ -0,0 +1,30 @@
+package com.yan.padccustomviewassignment.ui.mvp.presenters
+
+import androidx.lifecycle.ViewModel
+import com.yan.padccustomviewassignment.data.models.TaskModel
+import com.yan.padccustomviewassignment.data.models.TaskModelImpl
+import com.yan.padccustomviewassignment.ui.mvp.views.TaskListView
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class TaskListPresenterImpl : TaskListPresenter, ViewModel() {
+    private val mTaskModel: TaskModel = TaskModelImpl
+    private lateinit var mView: TaskListView
+
+    override fun onUiReady() {
+        mTaskModel.getTasks().subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                mView.displayTaskList(it)
+            }
+
+    }
+
+    override fun initView(view: TaskListView) {
+        mView = view
+    }
+
+    override fun onTapProfile() {
+
+    }
+}
