3f2ccba04cec9480ddc2b5e5a071955ca96ba8c5
yan-naing-phyo <yannaingphyo.dev@gmail.com>
2023-04-21 15:28:36 +0630

    Add MainView.kt

--- app/src/main/java/com/yan/padccustomviewassignment/ui/mvp/views/MainView.kt
+++ app/src/main/java/com/yan/padccustomviewassignment/ui/mvp/views/MainView.kt
@@ -1,7 +1,11 @@
 package com.yan.padccustomviewassignment.ui.mvp.views
 
+import com.yan.padccustomviewassignment.data.vos.ProjectVO
 import com.yan.padccustomviewassignment.data.vos.TaskVO
 
 interface MainView {
+    fun displayProjectData(projectVO: ProjectVO)
     fun displayTasks(tasks: List<TaskVO>)
+    fun showProfileDialog()
+    fun navigateToCreateTaskScreen()
 }
