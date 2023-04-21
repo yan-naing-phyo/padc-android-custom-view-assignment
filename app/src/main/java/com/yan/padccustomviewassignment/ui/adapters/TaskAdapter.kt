f238ce921cf01ddb9683c707507c19d0d5a7217b
yan-naing-phyo <yannaingphyo.dev@gmail.com>
2023-04-21 15:30:05 +0630

    Update TaskAdapter.kt

--- app/src/main/java/com/yan/padccustomviewassignment/ui/adapters/TaskAdapter.kt
+++ app/src/main/java/com/yan/padccustomviewassignment/ui/adapters/TaskAdapter.kt
@@ -6,14 +6,17 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import com.yan.padccustomviewassignment.data.vos.TaskVO
 import com.yan.padccustomviewassignment.databinding.ViewHolderTaskBinding
+import com.yan.padccustomviewassignment.ui.delegates.ProfileDelegate
 import com.yan.padccustomviewassignment.ui.viewholders.TaskViewHolder
 
-class TaskAdapter : ListAdapter<TaskVO, TaskViewHolder>(TaskDiffUtil()) {
+class TaskAdapter(
+    private val mProfileDelegate: ProfileDelegate
+) : ListAdapter<TaskVO, TaskViewHolder>(TaskDiffUtil()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val binding = ViewHolderTaskBinding.inflate(
             LayoutInflater.from(parent.context), parent, false
         )
-        return TaskViewHolder(binding)
+        return TaskViewHolder(binding, mProfileDelegate = mProfileDelegate)
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
